131ec7f3d4b02a32499d9b0557475341b98865f1
Timothy McMullan <mcmult@gmail.com>
2012-12-07 20:52:52 -0500

    back to single thread

--- problem2.java
+++ problem2.java
@@ -301,8 +301,8 @@ public class problem2 {
     private static final int Z = constants.Z;
     private static final int numLetters = constants.numLetters;
     private static Account[] accounts;
-    private static ExecutorService e = Executors.newFixedThreadPool(26);
-//    private static ExecutorService e = Executors.newSingleThreadExecutor();
+//    private static ExecutorService e = Executors.newFixedThreadPool(26);
+    private static ExecutorService e = Executors.newSingleThreadExecutor();
 
     private static void dumpAccounts() {
         // output values:
